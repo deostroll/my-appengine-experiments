663007a277095d25c1a3326ea7ffe9ad0e68a750
arun.jayapal <deostroll@gmail.com>
2012-07-17 02:02:42 +0530

    added code to talk to twitter and make a post

--- my-appengine-experiments/thewebsink/main.py
+++ my-appengine-experiments/thewebsink/main.py
@@ -17,20 +17,52 @@
 import webapp2
 import jinja2
 import os
+import oauth
+import logging
+from google.appengine.api import urlfetch
+
 je = jinja2.Environment(
 				loader=jinja2.FileSystemLoader(os.path.dirname(__file__)))
-				
+
+consumer_key = 'Osq1xe7gL1PE0mg2kUeuxw'
+consumer_secret = '305fZthlfLeTxheS4x7lL9D0jV63fCXMkGnc302Y'
+callback_url = ''
+
 class MainHandler(webapp2.RequestHandler):
     def get(self):
 		tl = je.get_template('login.htm')
 		self.response.out.write(tl.render({}))
 
-
+class twitter_redirect(webapp2.RequestHandler):
+	def get(self):
+		url = self.request.url
+		cb = url[:url.rfind('/')] + '/oauthHandler'
+		self.response.out.write(cb)
+		global callback_url 
+		callback_url = cb
+		client = oauth.TwitterClient(consumer_key, consumer_secret, cb)
+		self.redirect(client.get_authorization_url())
+		
 class oauthHandler(webapp2.RequestHandler):
 	def get(self):
-		self.response.out.write("Welcome...")
-		self.response.out.write("\n")
-		self.response.out.write("Oauth Token = %s" % 'test')
+		client = oauth.TwitterClient(consumer_key, consumer_secret, callback_url)
+		token = self.request.get('oauth_token')
+		verifier = self.request.get('oauth_verifier')
+		user_info = client.get_user_info(token, auth_verifier=verifier)
+		logging.debug('THE user_info object dump: %s' % str(user_info))
+		twparams = {
+			'status' : 'This is hello from The Web Sink',
 		
-app = webapp2.WSGIApplication([('/', MainHandler)],
+			}
+		result = client.make_request(
+				'http://twitter.com/statuses/update.json',
+				token = user_info['token'],
+				secret = user_info['secret'],
+				additional_params = twparams,
+				method = urlfetch.POST)		
+		self.response.out.write("Posted something...")
+		
+app = webapp2.WSGIApplication([('/', MainHandler),
+							('/redirect', twitter_redirect),
+							('/oauthHandler' , oauthHandler)],
                               debug=True)
